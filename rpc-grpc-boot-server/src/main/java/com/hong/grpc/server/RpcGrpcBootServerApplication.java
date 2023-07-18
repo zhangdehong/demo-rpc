a24354ddc008f433e6a9c2c07008025bb7c3dc30
晴天 <9324209+z-de-hong@user.noreply.gitee.com>
2023-07-19 00:33:26 +0800

    提交grpc boot starter 模块

---
+++ rpc-grpc-boot-server/src/main/java/com/hong/grpc/server/RpcGrpcBootServerApplication.java
@@ -0,0 +1,13 @@
+package com.hong.grpc.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcGrpcBootServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpcGrpcBootServerApplication.class, args);
+	}
+
+}
---
+++ rpc-grpc-boot-server/src/test/java/com/hong/grpc/server/RpcGrpcBootServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong.grpc.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcGrpcBootServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
